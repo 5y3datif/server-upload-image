0cc1cb6308cd01c558b83c273d5b5b8d370de008
5y3datif <63924971+5y3datif@users.noreply.github.com>
2021-12-07 15:26:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def upload():
         imagefile = request.files['image']
         filename = werkzeug.utils.secure_filename(imagefile.filename)
         print("\nReceived image File name : " + imagefile.filename)
-        imagefile.save("./uploadedimages/" + filename)
+        #imagefile.save("./uploadedimages/" + filename)
         
         return jsonify({
             "message": "Image Uploaded Successfully ",
